9e2c07c6a6a0ad571d412d14b7af6e31d46deb99
David Seidler <reldies.divad@gmail.com>
2023-10-19 20:00:15 +0200

    chore: add types workaround for images

---
+++ src/global.d.ts
@@ -0,0 +1,10 @@
+declare module "*&imagetools" {
+    /**
+     * imagetools (image processing plugin) triggers typescript errors
+     * actual types
+     * - code https://github.com/JonasKruckenberg/imagetools/blob/main/packages/core/src/output-formats.ts
+     * - docs https://github.com/JonasKruckenberg/imagetools/blob/main/docs/guide/getting-started.md#metadata
+     */
+    const out;
+    export default out;
+  }
