4a017632a52515c54007d38119d91ed4c6422d81
David Seidler <reldies.divad@gmail.com>
2023-11-01 18:55:24 +0100

    chore: clean up song filtering

--- src/routes/songs/[slug]/+page.ts
+++ src/routes/songs/[slug]/+page.ts
@@ -3,18 +3,21 @@ import { error } from '@sveltejs/kit';
 export async function load({ params }) {
 	try {
 		const song = await import(`../../../songbook/${params.slug}.md`);
-//		if (song.metadata.published !== true) {
-//			throw new Error('unpublished');
-//		}
+		// to solve later
+		// currently, i use URL parameter to toggle all songs / published songs
+		// should do it in svelte store and localstorage
+		//		if (song.metadata.published !== true) {
+		//			throw new Error('unpublished');
+		//		}
 		return {
 			content: song.default,
 			meta: song.metadata
 		};
 	} catch (e) {
 		console.info(e);
-		if (e.message === 'unpublished') {
-			throw error(401, 'Sorry, this content is not published yet.');
-		}
+		//		if (e.message === 'unpublished') {
+		//			throw error(401, 'Sorry, this content is not published yet.');
+		//		}
 		throw error(404, `Could not find ${params.slug}`);
 	}
 }
